81594090433b817dea9c71b6eb37f25adf6578f2
lifewater <jonei011@gmail'com>
2025-06-20 18:37:26 +0000

    basic chat message  and binary encoding check

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,6 +1,26 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"server/pkg/packets"
+
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
-	fmt.Println("hi")
+	// create chat packet
+	packet := &packets.Packet{
+		SenderId: 69,
+		Msg:      packets.NewChatMessage("Chat Message!"),
+	}
+
+	fmt.Println(packet)
+
+	// turn protobuf packet into primary data (aka convert packet to byte slice [byte])
+	data, err := proto.Marshal(packet)
+	if err != nil {
+		fmt.Println("Error marshaling packet: ", err)
+		return
+	}
+	fmt.Println(data)
 }
