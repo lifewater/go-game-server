03395f61d7d8a2924a1d391faca2cb720821a418
lifewater <jonei011@gmail'com>
2025-06-20 19:15:33 +0000

    series of packet tests

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,18 +9,27 @@ import (
 
 func main() {
 	// create chat packet
-	packet := &packets.Packet{
+	packet1 := &packets.Packet{
 		SenderId: 69,
 		Msg:      packets.NewChatMessage("Chat Message!"),
 	}
-
-	fmt.Println(packet)
+	fmt.Printf("Raw Data Packet:    %s\n", packet1)
 
 	// turn protobuf packet into primary data (aka convert packet to byte slice [byte])
-	data, err := proto.Marshal(packet)
+	pdata, err := proto.Marshal(packet1)
 	if err != nil {
 		fmt.Println("Error marshaling packet: ", err)
 		return
 	}
-	fmt.Println(data)
+	fmt.Printf("Marshal'd Packet:   %v\n", pdata)
+
+	// turn binary data back into protobuf packet
+	rdata := []byte{8, 69, 18, 15, 10, 13, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33}
+	packet2 := &packets.Packet{}
+	err2 := proto.Unmarshal(rdata, packet2)
+	if err2 != nil {
+		fmt.Println("Error unmarshaling packet:   ", err)
+		return
+	}
+	fmt.Printf("Unmarshal'd Packet: %v\n", packet2)
 }
