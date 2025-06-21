255691e9c99b40063c540990b0c591783d06722f
lifewater <jonei011@gmail.com>
2025-06-21 01:09:31 +0000

    initial start of server code

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"server/pkg/packets"
+	"log"
+	"net/http"
 
-	"google.golang.org/protobuf/proto"
+	"server/internal/server"
+	"server/internal/server/clients"
+)
+
+var (
+	port = flag.Int("port", 8080, "Port to listen on")
 )
 
 func main() {
-	// create chat packet
-	packet1 := &packets.Packet{
-		SenderId: 69,
-		Msg:      packets.NewChatMessage("Chat Message!"),
-	}
-	fmt.Printf("Raw Data Packet:    %s\n", packet1)
+	flag.Parse()
 
-	// turn protobuf packet into primary data (aka convert packet to byte slice [byte])
-	pdata, err := proto.Marshal(packet1)
+	// Define the game hub
+	hub := server.NewHub()
+
+	// Define handler for WebSocket connections
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		hub.Serve(clients.NewWebSocketClient, w, r)
+	})
+
+	// Start the server
+	go hub.Run()
+	addr := fmt.Sprintf(":%d", *port)
+
+	log.Printf("Starting server on %s", addr)
+	err := http.ListenAndServe(addr, nil)
+
 	if err != nil {
-		fmt.Println("Error marshaling packet: ", err)
-		return
+		log.Fatalf("Failed to start server: %v", err)
 	}
-	fmt.Printf("Marshal'd Packet:   %v\n", pdata)
 
-	// turn binary data back into protobuf packet
-	rdata := []byte{8, 69, 18, 15, 10, 13, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33}
-	packet2 := &packets.Packet{}
-	err2 := proto.Unmarshal(rdata, packet2)
-	if err2 != nil {
-		fmt.Println("Error unmarshaling packet:   ", err)
-		return
-	}
-	fmt.Printf("Unmarshal'd Packet: %v\n", packet2)
 }
